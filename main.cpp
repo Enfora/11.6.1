58e9302b0e3490c234e239dac770db76a57a8513
Enfora <enfora@mail.ru>
2022-10-05 01:11:32 +0500

    1

--- main.cpp
+++ main.cpp
@@ -5,15 +5,9 @@ using namespace std;
 int main(){
     setlocale(LC_ALL, "Russian");
     cout << "Test Primer"<< endl;
-    cout << "Test Primer"<< endl;
-    cout << "Test Primer"<< endl;
-    cout << "Test Primer"<< endl;
-    cout << "Test Primer"<< endl;
-    cout << "Test Primer"<< endl;
-    cout << "Test Primer1"<< endl;
-    cout << "Test Primer1"<< endl;
-    cout << "Test Primer2"<< endl;
-    cout << "Test Primer2"<< endl;
+
+    
+
 
     system("PAUSE");
 }
